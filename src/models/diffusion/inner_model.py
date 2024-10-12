dbf79ec7b50ec12368e6f6b4938376217c4b26bc
Batuhan Taskaya <isidentical@gmail.com>
2024-10-12 05:36:18 +0300

    Update inner_model.py

--- src/models/diffusion/inner_model.py
+++ src/models/diffusion/inner_model.py
@@ -45,10 +45,8 @@ class InnerModel(nn.Module):
 
     def forward(self, noisy_next_obs: Tensor, c_noise: Tensor, c_noise_cond: Tensor, obs: Tensor, act: Optional[Tensor]) -> Tensor:
         if self.act_emb is not None:
-            assert act.ndim == 2 or (act.ndim == 3 and act.size(2) == self.act_emb[0].num_embeddings and set(act.unique().tolist()).issubset(set([0, 1])))
             act_emb = self.act_emb(act) if act.ndim == 2 else self.act_emb[1]((act.float() @ self.act_emb[0].weight))
         else:
-            assert act is None
             act_emb = 0
         cond = self.cond_proj(self.noise_emb(c_noise) + self.noise_cond_emb(c_noise_cond) + act_emb)
         x = self.conv_in(torch.cat((obs, noisy_next_obs), dim=1))
